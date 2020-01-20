cf746a79e1f5fb85e1de848857158336099f0692
Arun <ajoshi@adsworks.com>
2020-01-20 18:02:23 -0500

    navBar changes

--- src/components/reusable-components/Footer.js
+++ src/components/reusable-components/Footer.js
@@ -19,20 +19,6 @@ const Footer = () => {
 		
 		
             <div className="flex-1">
-                <p className="uppercase text-gray-500 md:mb-6">Links</p>
-                <ul className="list-reset mb-6">
-                    <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="/" className="no-underline hover:underline text-gray-800 hover:text-orange-500">FAQ</a>
-                    </li>
-                    <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="/" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Help</a>
-                    </li>
-                    <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="/" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Support</a>
-                    </li>
-                </ul>
-            </div>
-            <div className="flex-1">
                 <p className="uppercase text-gray-500 md:mb-6">Legal</p>
                 <ul className="list-reset mb-6">
                     <li className="mt-2 inline-block mr-2 md:block md:mr-0">
